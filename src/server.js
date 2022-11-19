78f5bf28cbf5cc9fc4000b1def5836c1fe2d66c4
wito js <witoirawan.js@gmail.com>
2022-11-19 23:11:13 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     //ada error solusinya di chrome masukan setting address: chrome://flags/#block-insecure-private-network-requests setting cors ke disabled, lalu ubah lagi jadi default ketika sudah mencoba notes app nya
     routes: {
       cors: {
